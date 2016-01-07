3448cc411256838d1321efacac8c0d801ca30f5d
Fahim Zubayer <fahimzubayer18@gmail.com>
2016-01-07 15:14:51 +0600

    pp

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
-	print "<h2>POLTOOSH!</h2>";
-	print "<h1>Something is wrong with the XAMPP installation :-(</h1>";
+$end="";
+if($end=="")
+{
+echo '
+		<div id="login">
+		<form method="POST" action="login.php">
+		<h4>Location?</h4>
+		Username <input type="text" name="u" value=""> <br>
+		<input type="submit" value="Submit">
+		</form>
+		</div>
+		<br>
+		';
+
+$basicURL = "http://maps.googleapis.com/maps/api/geocode/json?address=";
+$end = urlencode($_POST['u']);
+}
+if($end)
+{
+	$finalURL=$basicURL . $end;
+	$homepage = file_get_contents($finalURL);
+	$data = json_decode($homepage);
+	echo $homepage;
+
+	#echo $data['status'];
+	$end="";
+}
 ?>
