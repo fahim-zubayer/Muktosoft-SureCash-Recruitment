09747049981c8928b82110184d70fb643e309efd
Fahim Zubayer <fahimzubayer18@gmail.com>
2016-01-10 17:04:10 +0600

    d

--- authority.php
+++ authority.php
@@ -12,10 +12,10 @@ $settings = array(
 
 
 $id=$_GET['twitter_id'];
-$SPAN=$_GET['time_span'];
+$SPAN=$_GET['tweet'];
 
 $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-$getfield = '?screen_name='.$id.'&count=200';
+$getfield = '?screen_name='.$id.'&count=5';
 $requestMethod = 'GET';
 $twitter = new TwitterAPIExchange($settings);
 $data= $twitter->setGetfield($getfield)
@@ -24,74 +24,14 @@ $data= $twitter->setGetfield($getfield)
 
 $da=json_decode($data,true);
 
-$day=array();
-$hr=array();
-$M=array();
-
-$M['Sun']='0'; $M['Mon']='1'; $M['Tue']='2'; $M['Wed']='3'; $M['Thu']='4'; $M['Fri']='5'; $M['Sat']='6';
-
-$dayy="";
-$dcnt=0;
-
-$ha="";
-$hcnt=0;
+$word_list=array();
 
 for($x=0;$x<count($da);$x++)
 {
-	$st=$da[$x]["created_at"];
-	$d=$st[0].$st[1].$st[2];
-	$d=$M[$d];
-	
-	if(!isset($day[$d]))
-		{
-			$day[$d]=1;
-		}
-		else
-		{
-			$c=$day[$d];
-			$c++;
-			$day[$d]=$c;
-		}
-	
-	if($day[$d]>$dcnt)
-	{
-		$dcnt=$day[$d];
-		$dayy=$d;
-	}
-	
-	
-	$t=$st[11].$st[12];
-	
-	if(!isset($hr[$t]))
-		{
-			$hr[$t]=1;
-		}
-		else
-		{
-			$c=$hr[$t];
-			$c++;
-			$hr[$t]=$c;
-		}
-	
-	if($hr[$t]>$hcnt)
-	{
-		$hcnt=$hr[$t];
-		$ha=$t;
-	}
-	
-	//echo $st."<br>";
-	//echo $d." ".$t."<br>";
-	
+	$st=$da[$x]["text"];
+	echo $st;
 	//echo "-------<br>";
 }
 
-if($SPAN=='hour')
-{
-	print(json_encode(array($ha=>$hcnt)));
-}
-else
-{
-	print(json_encode(array($dayy=>$dcnt)));
-}
 
 ?>
