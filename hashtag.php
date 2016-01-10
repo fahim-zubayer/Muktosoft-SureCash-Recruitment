29643e41f83d72fbb795ed1d473816890b73569b
Fahim Zubayer <fahimzubayer18@gmail.com>
2016-01-10 12:38:20 +0600

    dd

--- hashtag.php
+++ hashtag.php
@@ -12,26 +12,10 @@ $settings = array(
 
 /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
 
-$url = 'https://api.twitter.com/1.1/blocks/create.json';
-$requestMethod = 'POST';
-
-/** POST fields required by the URL above. See relevant docs as above **/
-$postfields = array(
-    'screen_name' => 'usernameToBlock', 
-    'skip_status' => '1'
-);
-
-/** Perform a POST request and echo the response **/
-/*
-$twitter = new TwitterAPIExchange($settings);
-echo $twitter->buildOauth($url, $requestMethod)
-             ->setPostfields($postfields)
-             ->performRequest();
-*/
 /** Perform a GET request and echo the response **/
 /** Note: Set the GET field BEFORE calling buildOauth(); **/
 $url = 'https://api.twitter.com/1.1/followers/ids.json';
-$getfield = '?screen_name='.$_GET['screen_name'];
+$getfield = '?screen_name=J7mbo';
 $requestMethod = 'GET';
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setGetfield($getfield)
